df25fd30fde8680183d5ce714d161dfdf423eeeb
flying-adv <123318831+flying-adv@users.noreply.github.com>
2023-01-25 03:58:32 +0000

    added images

--- inference.sh
+++ inference.sh
@@ -1,30 +1,50 @@
 
 #### HUMAN FACES ###########
-# python ./scripts/inference.py \
-# --images_dir=./CelebA-HQ-img   --edit_attribute='inversion' \
-# --save_dir=./results  ./pretrained_models/iteration_14000.pt
+python ./scripts/inference.py \
+--images_dir=./imgs/faces   --edit_attribute='inversion' \
+--save_dir=./results  ./iteration_14000.pt
 
 # python ./scripts/inference.py \
-# --images_dir=./test_imgs  --n_sample=100 --edit_attribute='lip'  \
-# --save_dir=./experiment/inference_results    ./checkpoint/ckpt.pt 
+# --images_dir=./imgs/faces  --n_sample=100 --edit_attribute='lip'  \
+# --save_dir=./results ./iteration_14000.pt
 
 # python ./scripts/inference.py \
-# --images_dir=./test_imgs  --n_sample=100 --edit_attribute='beard'  \
-# --save_dir=./experiment/inference_results  ./checkpoint/ckpt.pt 
+# --images_dir=./imgs/faces  --n_sample=100 --edit_attribute='beard'  \
+# --save_dir=./results  ./iteration_14000.pt
 
 # python ./scripts/inference.py \
-# --images_dir=./test_imgs  --n_sample=100 --edit_attribute='eyes'  \
-# --save_dir=./experiment/inference_results  ./checkpoint/ckpt.pt 
+# --images_dir=./imgs/faces  --n_sample=100 --edit_attribute='eyes'  \
+# --save_dir=./results  ./iteration_14000.pt
 
 # python ./scripts/inference.py \
-# --images_dir=./test_imgs  --n_sample=100 --edit_attribute='smile' --edit_degree=1.0  \
-# --save_dir=./experiment/inference_results  ./checkpoint/ckpt.pt 
+# --images_dir=./imgs/faces  --n_sample=100 --edit_attribute='smile' --edit_degree=1.0  \
+# --save_dir=./results  ./iteration_14000.pt
 
 # python ./scripts/inference.py \
-# --images_dir=./test_imgs  --n_sample=100 --edit_attribute='age' --edit_degree=3  \
-# --save_dir=./experiment/inference_results  ./checkpoint/ckpt.pt 
+# --images_dir=./imgs/faces  --n_sample=100 --edit_attribute='age' --edit_degree=3  \
+# --save_dir=./results  ./iteration_14000.pt
 
 #### CAR ######
 # python ./scripts/inference_cars.py \
-# --images_dir=./cars_train/cars_test --n_sample=500  --edit_attribute='Grass' \
-# --save_dir=./test_results/cars  ./pretrained_models/iteration_6000.pt
+# --images_dir=./imgs/cars --n_sample=100  --edit_attribute='inversion' \
+# --save_dir=./results  ./iteration_6000.pt
+
+# python ./scripts/inference_cars.py \
+# --images_dir=./imgs/cars --n_sample=100  --edit_attribute='Pose1' \
+# --save_dir=./results  ./iteration_6000.pt
+
+# python ./scripts/inference_cars.py \
+# --images_dir=./imgs/cars --n_sample=100  --edit_attribute='Pose2' \
+# --save_dir=./results  ./iteration_6000.pt
+
+# python ./scripts/inference_cars.py \
+# --images_dir=./imgs/cars --n_sample=100  --edit_attribute='cube' \
+# --save_dir=./results  ./iteration_6000.pt
+
+# python ./scripts/inference_cars.py \
+# --images_dir=./imgs/cars --n_sample=100  --edit_attribute='color' \
+# --save_dir=./results  ./iteration_6000.pt
+
+# python ./scripts/inference_cars.py \
+# --images_dir=./imgs/cars --n_sample=100  --edit_attribute='Grass' \
+# --save_dir=./results  ./iteration_6000.pt
