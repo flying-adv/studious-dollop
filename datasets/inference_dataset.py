ba56148de3a0a84cf6abea863b355d4bca5790f9
flying-adv <123318831+flying-adv@users.noreply.github.com>
2023-08-08 17:32:26 +0530

    Update inference_dataset.py

--- datasets/inference_dataset.py
+++ datasets/inference_dataset.py
@@ -5,21 +5,25 @@ from utils import data_utils
 
 class InferenceDataset(Dataset):
 
-	def __init__(self, root, opts, transform=None, preprocess=None):
-		self.paths = sorted(data_utils.make_dataset(root))
-		self.transform = transform
-		self.preprocess = preprocess
-		self.opts = opts
+    def __init__(self, root, opts, transform=None, preprocess=None):
+        self.paths = sorted(data_utils.make_dataset(root))
+        self.transform = transform
+        self.preprocess = preprocess
+        self.opts = opts
 
-	def __len__(self):
-		return len(self.paths)
+    def __len__(self):
+        return len(self.paths)
 
-	def __getitem__(self, index):
-		from_path = self.paths[index]
-		if self.preprocess is not None:
-			from_im = self.preprocess(from_path)
-		else:
-			from_im = Image.open(from_path).convert('RGB')
-		if self.transform:
-			from_im = self.transform(from_im)
-		return from_im
+    def __getitem__(self, index):
+        style_img = '/content/original.jpg'
+        from_path = self.paths[index]
+        if self.preprocess is not None:
+            from_im = self.preprocess(from_path)
+            style_img = self.preprocess(style_img)
+        else:
+            from_im = Image.open(from_path).convert('RGB')
+            style_img = Image.open(style_img).convert('RGB')
+        if self.transform:
+            from_im = self.transform(from_im)
+            style_img = self.transform(style_img)
+        return from_im,style_img
